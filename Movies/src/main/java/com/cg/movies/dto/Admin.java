bf212f3312370a5d240a20183a9ffdea4e874e56
Admin <Admin@192.168.1.9>
2019-09-21 11:55:22 +0530

    updates in admin

---
+++ Movies/src/main/java/com/cg/movies/dto/Admin.java
@@ -0,0 +1,85 @@
+package com.cg.movies.dto;
+
+public class Admin {
+	private Integer adminId;
+	private String adminName;
+	private String adminPassword;
+	
+	Admin(){
+		
+	}
+
+	public Admin(Integer adminId, String adminName, String adminPassword) {
+		super();
+		this.adminId = adminId;
+		this.adminName = adminName;
+		this.adminPassword = adminPassword;
+	}
+
+	public Integer getAdminId() {
+		return adminId;
+	}
+
+	public void setAdminId(Integer adminId) {
+		this.adminId = adminId;
+	}
+
+	public String getAdminName() {
+		return adminName;
+	}
+
+	public void setAdminName(String adminName) {
+		this.adminName = adminName;
+	}
+
+	public String getAdminPassword() {
+		return adminPassword;
+	}
+
+	public void setAdminPassword(String adminPassword) {
+		this.adminPassword = adminPassword;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((adminId == null) ? 0 : adminId.hashCode());
+		result = prime * result + ((adminName == null) ? 0 : adminName.hashCode());
+		result = prime * result + ((adminPassword == null) ? 0 : adminPassword.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Admin other = (Admin) obj;
+		if (adminId == null) {
+			if (other.adminId != null)
+				return false;
+		} else if (!adminId.equals(other.adminId))
+			return false;
+		if (adminName == null) {
+			if (other.adminName != null)
+				return false;
+		} else if (!adminName.equals(other.adminName))
+			return false;
+		if (adminPassword == null) {
+			if (other.adminPassword != null)
+				return false;
+		} else if (!adminPassword.equals(other.adminPassword))
+			return false;
+		return true;
+	}
+
+	@Override
+	public String toString() {
+		return "Admin [adminId=" + adminId + ", adminName=" + adminName + ", adminPassword=" + adminPassword + "]";
+	}
+	
+}
